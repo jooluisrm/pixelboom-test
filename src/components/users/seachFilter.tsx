52ed1b1a05c07cecb0066863f7380c6ee339ad11
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-10 13:04:37 -0300

    correçao

--- src/components/users/seachFilter.tsx
+++ src/components/users/seachFilter.tsx
@@ -9,7 +9,7 @@ export const SearchFilter = () => {
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground" size={16} />
                 <Input
                     placeholder="Buscar..."
-                    className="pl-9 pr-3 py-2 rounded-md h-10 w-full"
+                    className="pl-9 pr-3 py-2 rounded-md h-10 w-full border"
                 />
             </div>
             <IconItem type="filter" />
